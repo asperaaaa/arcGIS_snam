9273184c38b9c6b043967559c333a4a75dc46522
michele.abbrescia <michele.abbrescia@accenture.com>
2021-11-23 11:09:49 +0100

    fix map constraints

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-require(["esri/config", "esri/Map", "esri/views/MapView", "esri/layers/GeoJSONLayer", "esri/Basemap"], function (
+require(["esri/config", "esri/Map", "esri/views/MapView", "esri/layers/GeoJSONLayer", "esri/Basemap","esri/views/SceneView"], function (
   esriConfig,
   Map,
   MapView,
   GeoJSONLayer,
-  Basemap
+  Basemap,
+  SceneView
 ) {
   esriConfig.apiKey =
     "AAPK458453f872f04d9883da057b3cf03fd9MtYiqYcCKy61WkYFI1ySlxP2u5WcoIkzfswoHiArIWHaDMyRWDgAX7Xa-pxhh7Zy";
@@ -51,14 +52,24 @@ require(["esri/config", "esri/Map", "esri/views/MapView", "esri/layers/GeoJSONLa
     layers: [geojsonLayer]
   });
 
-  view = new MapView({
+  // view = new MapView({
+  //   map: map,
+  //   center: [13.013, 42.026], // Longitude, latitude
+  //   zoom: 5, // Zoom level
+  //   container: "viewDiv", // Div element
+  // });
+
+  view = new SceneView({
+    container: "viewDiv",
     map: map,
-    center: [13.013, 42.026], // Longitude, latitude
-    zoom: 5, // Zoom level
-    container: "viewDiv", // Div element
+    scale: 9140598.13105315,
+    center: [12.5538812361089167, 41.07347248726621],
+    constraints: {
+      rotationEnabled: false
+    }
   });
 
-  map.allLayers.forEach(layer => {console.log(layer.title)});
+  // map.allLayers.forEach(layer => {console.log(layer.title)});
 
   // Set up a click event handler and retrieve the screen x, y coordinates
   view.on("click", function(event) {
@@ -77,5 +88,66 @@ require(["esri/config", "esri/Map", "esri/views/MapView", "esri/layers/GeoJSONLa
       center: graphic.graphic.geometry,
       zoom: 15
     });  // Sets the center point of the view at a specified lon/lat
+  }
+
+  view.when(disableZooming);
+  
+  /**
+   * Disables all zoom gestures on the given view instance.
+   *
+   * @param {esri/views/MapView} view - The MapView instance on which to
+   *                                  disable zooming gestures.
+   */
+  function disableZooming(view) {
+    view.popup.dockEnabled = true;
+
+    // Removes the zoom action on the popup
+    view.popup.actions = [];
+
+    // stops propagation of default behavior when an event fires
+    function stopEvtPropagation(event) {
+      event.stopPropagation();
+    }
+
+    // exlude the zoom widget from the default UI
+    view.ui.components = ["attribution"];
+
+    // disable mouse wheel scroll zooming on the view
+    view.on("mouse-wheel", stopEvtPropagation);
+
+    // disable zooming via double-click on the view
+    view.on("double-click", stopEvtPropagation);
+
+    // disable zooming out via double-click + Control on the view
+    view.on("double-click", ["Control"], stopEvtPropagation);
+
+    // disables pinch-zoom and panning on the view
+    view.on("drag", stopEvtPropagation);
+
+    // disable the view's zoom box to prevent the Shift + drag
+    // and Shift + Control + drag zoom gestures.
+    view.on("drag", ["Shift"], stopEvtPropagation);
+    view.on("drag", ["Shift", "Control"], stopEvtPropagation);
+
+    // prevents zooming with the + and - keys
+    view.on("key-down", (event) => {
+      const prohibitedKeys = [
+        "+",
+        "-",
+        "Shift",
+        "_",
+        "=",
+        "ArrowUp",
+        "ArrowDown",
+        "ArrowRight",
+        "ArrowLeft"
+      ];
+      const keyPressed = event.key;
+      if (prohibitedKeys.indexOf(keyPressed) !== -1) {
+        event.stopPropagation();
+      }
+    });
+
+    return view;
   }
 });
